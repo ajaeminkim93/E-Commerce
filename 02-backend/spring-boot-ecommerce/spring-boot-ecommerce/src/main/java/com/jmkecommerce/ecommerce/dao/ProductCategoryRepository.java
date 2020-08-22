83b85f6566b55e6db17d9b594c1922a956e11a95
Jae(Andrew) Kim <jae.m.kim@wsu.edu>
2020-08-21 20:43:51 -0700

    Created cross origin annotation into JPA repositories. Fixed CORS error.

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/jmkecommerce/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/jmkecommerce/ecommerce/dao/ProductCategoryRepository.java
@@ -3,9 +3,11 @@ package com.jmkecommerce.ecommerce.dao;
 import com.jmkecommerce.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // data access object                           // Name of JSON Entry, path to product-category
 // provides interface or persistence mechanism
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/jmkecommerce/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/jmkecommerce/ecommerce/dao/ProductRepository.java
@@ -2,10 +2,11 @@ package com.jmkecommerce.ecommerce.dao;
 
 import com.jmkecommerce.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // repository is an interface that allows developer to design  a data access contract
 // The Spring Data JPA framework can then inspect that contract, and automatically build the interface implementation under the covers for you
-                                            // <Entity type, Primary Key type>
+@CrossOrigin("http://localhost:4200")                                            // <Entity type, Primary Key type>
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
