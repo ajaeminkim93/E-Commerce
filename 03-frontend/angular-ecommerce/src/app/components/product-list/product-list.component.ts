a0669f6d7e39ef9b7100336beacd3f7073cc8f30
Jae(Andrew) Kim <jae.m.kim@wsu.edu>
2020-08-21 20:22:17 -0700

    Inject productService into our product-list component so that it can read the json via the service. Then we map the data we retrieve to our products array in our component.

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
+
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
 
-  ngOnInit(): void {
+  constructor(
+    private productService: ProductService // injecting ProductService into this component.
+    ) { }
+
+  ngOnInit() {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {                     // when data is returned we assign to our own properties.
+        this.products = data;       // assign the data retrieved into our products array.
+      }
+    )
   }
 
 }
